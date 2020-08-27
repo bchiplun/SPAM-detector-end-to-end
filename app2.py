aad28fb787b1333f6435d455f6e0f7fb6998e0af
Bhavesh3422 <bchiplunkar@gmail.com>
2020-08-28 00:08:19 +0530

    updating actual app

---
+++ app2.py
@@ -0,0 +1,33 @@
+#app to deploy model and predict the spam
+
+#importing important libraries
+
+from flask import Flask, request, jsonify, render_template
+from clean_text import clean_mesaage
+import pickle
+
+app = Flask(__name__) #Creating app
+
+#Loading models from pickle
+
+classifier = pickle.load(open('classifier_model.pkl','rb'))
+cv = pickle.load(open('countverctorizer.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method=='POST':
+        message = request.form['message']
+        message = clean_mesaage(message)
+        message = [message]
+        vect = cv.transform(message)
+        my_prediction = classifier.predict(vect)
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(port=6011)
+    #app.run(debug=True)
+
